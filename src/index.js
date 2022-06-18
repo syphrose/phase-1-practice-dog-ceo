b5c4efbce8db93a95fa9cc4041b5d9c68eb25924
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-18 22:22:47 +0300

    commit tree 054f209ccbae9a87ab4cdf88cb015a0c55b8d26f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+document.addEventListener("DOMContentLoaded",
+function() {
+    fetchDogImages()
+})
+
+function fetchDogImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(data => {
+        let image = data.message
+        fetchDogImages.forEach(image => {
+            loadImagestoDOM(image)
+        });
+
+        function loadImagestoDOM(image) {
+            let dogsCont = document.getElementById("dog-i")
+            let ourImage = document.createElement('image')
+            ourImage.src = image
+            dogsCont.appendChild(ourImage)
+        }
+    })
+}
